3c3917ddbb1dc51dfe322ffda4f6b69d23cdf1f3
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-06-11 02:53:16 -0400

    Add stock rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpg::Application.config.secret_token = 'ba938ac1b01ad303ba7ba286a5b19efaf93f6e4edc0799f78fa47b1bd295908d7240d3f9ac0d73d7aa359f1ad69bca8608c6aa4add865454e62dd0ddba72b604'
