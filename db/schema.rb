df5af7e3fe5c47da67d05a19f2d947776a495e56
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-06-13 19:31:35 -0400

    Run game migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612003053) do
+ActiveRecord::Schema.define(:version => 20130613233035) do
+
+  create_table "games", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "active"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
