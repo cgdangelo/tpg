ed1fb4fcc261aa5bb5fbd165cc113fa6d023f4bb
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-06-20 02:56:54 -0400

    Deactivate leagues when game is deactivated

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,15 @@
 class Game < ActiveRecord::Base
-  attr_accessible :active, :description, :name
-  validates_presence_of :active, :description, :name
   resourcify
+  attr_accessible :active, :description, :name
+  validates_presence_of :description, :name
+  validates :active, :inclusion => { :in => [true, false] }
+  after_save :set_leagues_inactive
+
+  def set_leagues_inactive
+    if !self.active
+      League.where(:game_id => self.id).each do |league|
+        league.deactivate!
+      end
+    end
+  end
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -2,7 +2,8 @@ class League < ActiveRecord::Base
   resourcify
   belongs_to :game
   attr_accessible :active, :description, :name
-  validates_presence_of :active, :description, :name
+  validates_presence_of :description, :name
+  validates :active, :inclusion => { :in => [true, false] }
 
   def add_admin(user)
     user.add_role :league_admin, self
@@ -10,5 +11,15 @@ class League < ActiveRecord::Base
 
   def remove_admin(user)
     user.remove_role :league_admin, self
+  end
+
+  def activate!
+    self.active = true
+    self.save
+  end
+
+  def deactivate!
+    self.active = false
+    self.save
   end
 end
