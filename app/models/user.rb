a12bea0380545937fa83ebc4f92329adfeac9860
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-06-27 08:52:24 -0400

    Add uniq validation for user aliases

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :alias
 
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :alias
 end
