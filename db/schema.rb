751845c495d67b5ad520926e9b31613788d8cac0
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-06-27 08:59:08 -0400

    Add team scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130621224905) do
+ActiveRecord::Schema.define(:version => 20130627125757) do
 
   create_table "divisions", :force => true do |t|
     t.string   "name"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(:version => 20130621224905) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
+
+  create_table "teams", :force => true do |t|
+    t.string   "name"
+    t.string   "tag"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
