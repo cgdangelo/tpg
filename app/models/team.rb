9c740abf74ad22e50fe2854c69cbdc774f2fefe3
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-07-26 20:19:56 -0400

    Add join password field to teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ActiveRecord::Base
   resourcify
-  attr_accessible :name, :tag
+  attr_accessible :name, :tag, :password, :password_confirmation
   belongs_to :league
+  has_secure_password
+  validates_presence_of :password, :on => :create
 end
