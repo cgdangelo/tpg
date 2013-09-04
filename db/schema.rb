42a941d2e50131f2d58536b303374e212cdeb9b8
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-09-03 23:12:29 -0400

    Run week migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130904023753) do
+ActiveRecord::Schema.define(:version => 20130904031058) do
 
   create_table "divisions", :force => true do |t|
     t.string   "name"
